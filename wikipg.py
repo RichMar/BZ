f579e0e3891fa12ed89ca0d39f4edbd39ec7c2fc
RM <30491118+RichMar@users.noreply.github.com>
2023-03-14 09:54:37 +0100

    Update wikipg.py

--- wikipg.py
+++ wikipg.py
@@ -3,24 +3,35 @@ import os
 import fileinput
 from pathlib import Path
 
+
 def import_csv(csvfilemame):
     data = []
     row_index = 0
-    with open('statistika.csv', 'r') as stat:
+    r = 0
+    with open(csvfilemame, 'r') as stat:
         stat_reader = csv.reader(stat, delimiter=',')
     # last_row = stat_reader[-1]
         for row in stat_reader:
             if row:  # avoid blank lines
                 row_index += 1
-                columns = [str(row_index), row[0], row[1], row[2]]
+                try:
+                    columns = [str(row_index), row[0], row[1], row[2]]
+                    r = 1
+                except:
+                    columns = [str(row_index), row[0], row[1]]
+                    r = 2
                 data.append(columns)
-    return data
+    return data, r
 
 data = import_csv('statistika.csv')
-last_row = data[-1]
-boduvOSM = data[-1][3]
-chb = 2171-int(boduvOSM)
-b = data[-1][2]
+r = data[1]
+if r == 1:
+    last_row = data[0][-1]
+    bodyvOSM = data[0][-1][3]
+    chb = 2171 - int(bodyvOSM)
+    b = data[0][-1][2]
+data1 = import_csv('OSMbodychybejiciref.csv')
+proble = len(data1[0]) -1
 # absolute_path = os.path.dirname(__file__)
 # relative_path = "BZ.wiki\Home.md"
 # full_path = os.path.join(absolute_path, relative_path)
@@ -30,20 +41,24 @@ b = data[-1][2]
 print(Path.cwd())
 for root, dirs, files in os.walk(Path.cwd()):
     for name in files:
-        if name.endswith((".md")):
+        if name.endswith(("Home.md")):
             full_path = os.path.join(root, name)
             # print(root)
             # print(dirs)
             # print(files)
             print(full_path)
             for line in fileinput.input(full_path, inplace=True, encoding="cp852"):
-                if 'body" : ' in line:
+                if 'body" : ' in line and r == 1:
                     a = line.find('body" : ') + 8
                     x = line.replace(line[a:], str(chb))
                     print('{}'.format(x + '\n'), end='')
-                elif 'v OSM" : ' in line:
+                elif 'v OSM" : ' in line and r == 1:
                     a = line.find('v OSM" : ') + 9
-                    x = line.replace(line[a:], str(b))
+                    x = line.replace(line[a:], str(bodyvOSM))
+                    print('{}'.format(x + '\n'), end='')
+                elif 'otou" : ' in line:
+                    a = line.find('otou" : ') + 8
+                    x = line.replace(line[a:], str(proble))
                     print('{}'.format(x + '\n'), end='')
                 else:
                     print('{}'.format(line), end='')
